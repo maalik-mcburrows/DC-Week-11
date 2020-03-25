e4bfa5389d54906c4f24f72445f0bf5d29cd13a1
Maalik McBurrows <mmcburrows1@gmail.com>
2020-03-25 10:16:51 -0400

    Up to step 1 number 10 on memory card activity part 2

--- 03-24 Memory Ex./memory-game/src/components/MemoryCard.jsx
+++ 03-24 Memory Ex./memory-game/src/components/MemoryCard.jsx
@@ -5,8 +5,17 @@ class MemoryCard extends Component {
     render() {
         return (
             <div className="MemoryCard">
-                <img className="logo" src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"></img>
+                <div className="MemoryCardInner">
+                    <div className="MemoryCardBack">
+                        <img className="logo" src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"></img>
+                    </div>
+                    <div className="MemoryCardFront">
+                        ∆
+                    </div>
+                </div>
+                
             </div>
+                
             
         );
     }
