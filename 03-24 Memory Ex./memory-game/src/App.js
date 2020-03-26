77e9ae9934f95869dabf99484bede68f53c7fa67
Maalik McBurrows <mmcburrows1@gmail.com>
2020-03-26 12:55:39 -0400

    completed step 1

--- 03-24 Memory Ex./memory-game/src/App.js
+++ 03-24 Memory Ex./memory-game/src/App.js
@@ -3,25 +3,36 @@ import MemoryCard from './components/MemoryCard'
 import './App.css';
 
 function generateDeck() {
-  const symbols = "`∆`,` ß`, `£`, `§`,`•`, `$`, `+`, `ø`"
+  const symbols = [`∆`,` ß`, `£`, `§`,`•`, `$`, `+`, `ø`]
   deck = []
 
   for (let i = 0; i < 16; i++) {
-    i = {
-      isFlipped: false,
-      symbol: [i%8]
-    };
-    deck = deck + i
-  };
+    deck.push(
+      {
+        isFlipped: false,
+        symbol: symbols[i%8]
+      }
+    )
+  }
+
+  shuffle(deck);
+  return deck;
+}
+
+
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+  return a;
 }
 
 class App extends Component {
   state = {
-    deck: [], 
+    deck: generateDeck(), 
     pickedCards: []
   }
-
-  
 
   render(){
     return (
