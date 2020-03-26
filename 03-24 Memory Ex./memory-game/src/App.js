83d126885266a045cc52cd93ea26cc937e6eb690
Maalik McBurrows <mmcburrows1@gmail.com>
2020-03-26 10:48:59 -0400

    added constructor to app

--- 03-24 Memory Ex./memory-game/src/App.js
+++ 03-24 Memory Ex./memory-game/src/App.js
@@ -1,40 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MemoryCard from './components/MemoryCard'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Memory Game</h1>
-        <h2 className="App-subtitle">Match Cards to Win</h2>
-      </header>
-      <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Memory Game</h1>
+          <h2 className="App-subtitle">Match Cards to Win</h2>
+        </header>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div>
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
       </div>
-      <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-      </div>
-      <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-      </div>
-      <div>
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-        <MemoryCard />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
